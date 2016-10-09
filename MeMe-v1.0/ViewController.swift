1af9f4d6dc3c201f98e12b89b2e1ab83886e8193
LIJO RAJU <lijoraju@LIJOs-MacBook-Air.local>
2016-10-09 00:14:03 -0700

    Added default styles and text to top and bottom text fields

--- MeMe-v1.0/ViewController.swift
+++ MeMe-v1.0/ViewController.swift
@@ -13,32 +13,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
    
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
     
+    let memeAttributesForTextField = [NSStrokeColorAttributeName:UIColor.cyan, NSForegroundColorAttributeName:UIColor.black, NSFontAttributeName:UIFont(name: "HelveticaNeue-CondensedBlack",size:40)!, NSStrokeWidthAttributeName: -4.0] as [String : Any] //Intializing Dictionary for Default Attributes of Text Field
+    
+ //This method is the action for Pick Buttom which enables the use to select image froom Photo Library
     @IBAction func pickAnImageFromPhotoAlbum(_ sender: AnyObject) {
         
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
-        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary //Specifying Photo Library as Source
         present(imagePickerController, animated: true, completion: nil)
         
     }
     
+ //This method is the action for Camera Button which allows user to take a picture using camera and add
     @IBAction func pickAnImageFromCamera(_ sender: AnyObject) {
         
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
-        imagePickerController.sourceType = UIImagePickerControllerSourceType.camera
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.camera //Specifying Camera as Source
         present(imagePickerController, animated: true, completion: nil)
         
     }
     
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+  //This method sets image to UIImageView and dismiss the selection menu after selecting image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
         {
             imagePickerView.image = image
-            self.dismiss(animated: true, completion: nil)
+            self.dismiss(animated: true, completion: nil) // Dismiss the selection list
         }
         
     }
@@ -46,9 +52,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) //Enabling or Disabling Camera Button upon avialability of camera
         
         }
     
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        imagePickerView.contentMode = UIViewContentMode.scaleAspectFit
+        setTextField(topTextField,defaultText: "TOP")   //Intializing top TextField
+        setTextField(bottomTextField,defaultText: "BOTTOM")  //Intialing bottom TextField
+    }
+    
+  //This function sets default text and text styles to top and bottom textfields
+    func setTextField(_ textField:UITextField, defaultText:String){
+        
+        textField.text = defaultText
+        textField.defaultTextAttributes = memeAttributesForTextField
+        textField.textAlignment = NSTextAlignment.center
+    }
 }
 
