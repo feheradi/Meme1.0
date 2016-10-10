7b90c8bccaee7be5182d4fa34ac4768d41c5d6db
LIJO RAJU <lijoraju@LIJOs-MacBook-Air.local>
2016-10-10 12:36:42 -0700

    Rename MemeEditor back to ViewController

--- MeMe-v1.0/ViewController.swift
+++ MeMe-v1.0/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 //MARK: - ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
-class MemeEditorVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var bottomTextField: UITextField!
@@ -96,8 +96,8 @@ class MemeEditorVC: UIViewController, UIImagePickerControllerDelegate, UINavigat
   
     //This method use to subscribe keyboard notifications
     func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(MemeEditorVC.keyboardWillShow(_ :)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(MemeEditorVC.keyboardWillHide(_ :)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_ :)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_ :)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
     }
     
